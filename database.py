18944b73e34cbc24ae12518f77c18bb20dae9884
Rajith-2002 <bestusrajith@gmail.com>
2026-04-21 19:48:11 +0530

    added successfully

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "3306")
+DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
